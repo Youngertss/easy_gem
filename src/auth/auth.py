42a0aa1be93da6153254d0dc405e54f81fafad4c
Artur <bambukov17@gmail.com>
2025-07-04 02:35:55 +0300

    deposit is done; COOCKIES TRANSPORT

--- src/auth/auth.py
+++ src/auth/auth.py
@@ -5,21 +5,21 @@ from fastapi_users import FastAPIUsers
 from src.auth.manager import get_user_manager
 from src.auth.models import User
 
-# cookie_transport = CookieTransport(
-#     cookie_max_age=360000,
-#     # cookie_samesite=              "none",
-#     # cookie_secure=False
-#     )         
-bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")                                           
+cookie_transport = CookieTransport(
+    cookie_max_age=360000,
+    # cookie_samesite=              "none",
+    # cookie_secure=False
+    )         
+# bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")                                           
 
 SECRET = "SECRET"
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=360000)
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=bearer_transport,
+    transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
 
